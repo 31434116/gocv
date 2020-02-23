0bc09ce6f0fbcd34dfbc277d8a3abffd2e3ad8be
U-DESKTOP-P4B3RE3\Yoda <31434116@qq.com>
2020-02-24 00:27:29 +0800

    fix msys2 compile bug

--- cgo.go
+++ cgo.go
@@ -6,7 +6,7 @@ package gocv
 
 /*
 #cgo !windows pkg-config: opencv4
-#cgo CXXFLAGS:   --std=c++11
+#cgo CXXFLAGS:   --std=c++11 -Wno-conversion-null
 #cgo windows  CPPFLAGS:   -ID:/msys64/mingw64/include/opencv4
 #cgo windows  LDFLAGS:    -LD:/msys64/mingw64/lib -lopencv_core -lopencv_face -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_features2d -lopencv_video -lopencv_dnn -lopencv_xfeatures2d -lopencv_plot -lopencv_tracking -lopencv_img_hash -lopencv_calib3d
 */
